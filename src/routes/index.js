e3f660a14763562c3cf8289356cafff117e5c494
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2023-04-10 23:47:47 -0600

    Implemented new design on the login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,45 +1,50 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const passport = require('passport');
-
-router.get('/', (req, res, next) => {
-    res.render('login');
+const passport = require("passport");
+/**/
+router.get("/", (req, res, next) => {
+  res.render("login");
 });
 
-router.get('/signup', (req, res, next) => {
-    res.render('signup');
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
 });
 
-router.post('/signup', passport.authenticate('local-signup' ,{
-    successRedirect: '/',
-    failureRedirect: '/signup',
-    passReqToCallback: true
-}));
+router.post(
+  "/signup",
+  passport.authenticate("local-signup", {
+    successRedirect: "/",
+    failureRedirect: "/signup",
+    passReqToCallback: true,
+  })
+);
 
-router.post('/login', passport.authenticate('local-login' ,{
-    successRedirect: '/profile',
-    failureRedirect: '/',
-    passReqToCallback: true
-}));
+router.post(
+  "/login",
+  passport.authenticate("local-login", {
+    successRedirect: "/profile",
+    failureRedirect: "/",
+    passReqToCallback: true,
+  })
+);
 
-router.get('/profile', isAuthenticated, (req, res, next) => {
-    res.render('profile');
+router.get("/profile", isAuthenticated, (req, res, next) => {
+  res.render("profile");
 });
 
-router.get('/logout', (req, res, next) => {
-    req.logout(() => {
-        res.redirect('/');
-    });
+router.get("/logout", (req, res, next) => {
+  req.logout(() => {
+    res.redirect("/");
+  });
 });
 
 function isAuthenticated(req, res, next) {
-    if(req.isAuthenticated()){
-        return next();
-    }
+  if (req.isAuthenticated()) {
+    return next();
+  }
 
-    res.redirect('/');
+  res.redirect("/");
 }
-
 
 module.exports = router;
