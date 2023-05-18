26e31e881d1ef942a4c162d5171c4b7cf70b5b97
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2023-05-18 02:25:26 -0600

    Changed routesofthe index

--- src/routes/index.js
+++ src/routes/index.js
@@ -53,6 +53,28 @@ router.get("/listPerson", (req, res, next) => {
     });
 });
 
+router.get("/deletePerson/:id", function (req, res, next) {
+  Person.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err); // Se crea la funcion la cual encontrara y eliminara el objeto deseado
+    res.redirect("/listPerson"); // Se recarga la pagina para actualizarse
+  });
+});
+
+router.get("/findById/:id", function (req, res, next) {
+  Person.findById(req.params.id, function (err, person) {
+    if (err) return next(err); // Se crea la funcion la cual encontrara y se redirigira a la pagina de edicion
+    res.render("personUpdate", { person }); //Renderiza la pagina de edicion
+  });
+});
+
+router.get("/listProducto", (req, res, next) => {
+  Producto.find(function (err, producto) {
+    if (err) return next(err);
+    //res.json(person); Ahora en lugar de renderizar el json de person
+    res.render("productoIndex", { producto }); //Ejecutara el archivo 'personIndex' y tambien le envia el json
+  });
+}); //Se crea la ruta para ver el listo de registros en la coleccion
+
 router.get("/logout", (req, res, next) => {
   req.logout(() => {
     res.redirect("/");
